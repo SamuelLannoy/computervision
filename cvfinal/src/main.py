c4b8c0a9670b25885ff4e5c76c5e91fd860a3109
Samuel Lannoy <samuel.lannoy@student.kuleuven.be>
2014-04-30 11:46:07 +0200

    readLandmarks

--- cvfinal/src/main.py
+++ cvfinal/src/main.py
@@ -1,8 +1,13 @@
-'''
-Created on 28-apr.-2014
+import numpy as np
+import cv2
 
-@author: Samuel Lannoy, Vital D'haveloose
-'''
-
+def readLandmarks(toothId, nbPersons, nbLandmarks):
+    landmarks = np.zeros((nbLandmarks,nbPersons))
+    for personId in range(1, nbPersons+1):
+        f = open('../data/Landmarks/original/landmarks' + str(personId) + '-' + str(toothId) + '.txt', 'r')
+        for landmarkId in range(nbLandmarks): 
+            landmarks[landmarkId, personId-1] = float(f.readline())
+    return landmarks
+    
 if __name__ == '__main__':
-    pass
+    print readLandmarks(1, 14, 80)
