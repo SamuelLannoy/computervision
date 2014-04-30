bd69312fc23465bb3728209265798ad66c853377
Samuel Lannoy <samuel.lannoy@student.kuleuven.be>
2014-04-30 11:56:25 +0200

    whuteva

--- cvfinal/src/main.py
+++ cvfinal/src/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 def readLandmarks(toothId, nbPersons, nbLandmarks):
-    landmarks = np.zeros((nbLandmarks,nbPersons))
+    landmarks = np.zeros((nbLandmarks, nbPersons))
     for personId in range(1, nbPersons+1):
         f = open('../data/Landmarks/original/landmarks' + str(personId) + '-' + str(toothId) + '.txt', 'r')
         for landmarkId in range(nbLandmarks): 
