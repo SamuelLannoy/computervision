57e956340f14f413def636a044d888cd28a0f471
Vital D'haveloose <vital.dhaveloose@hotmail.be>
2014-05-01 22:21:11 +0200

    partially implemented procrustes in other file

--- cvfinal/src/main.py
+++ cvfinal/src/main.py
@@ -1,85 +1,6 @@
 import cv2
 import numpy as np
-
-'''
-Takes toothmatrix, and returns a similar one with, for every person, the landmarks in coordinates with an origin in the mean of those landmarks.
-'''
-def procrustesTranslateMatrix(matrix):
-    nbP = matrix.shape[1]
-    ret = np.zeros(matrix.shape)
-    for p in range(nbP):
-        ret[:,p,0] = procrustesTranslateVector(matrix[:,p,0])
-        ret[:,p,1] = procrustesTranslateVector(matrix[:,p,1])
-        
-    return ret #TODO: methode gebruikt?
-
-'''
-Takes numpy vector with the values for one persons, different x- or y values of landmarks, and returns a similar vector with the delta of each
-entry to the average over the landmarks.
-'''    
-def procrustesTranslateVector(vector):
-    nbL = vector.shape[0]
-    avg = np.mean(vector)
-    
-    ret = np.zeros(vector.shape)
-    for l in range(nbL):
-        ret[l] = vector[l] - avg
-    return ret
-
-'''
-Takes toothmatrix, and returns a similar one with, for every person, the landmarks in coordinates scaled so
-that the standard deviation of those landmarks becomes 1 (in both directions).
-'''
-def procrustesScaleMatrix(matrix):
-    nbP = matrix.shape[1]
-    ret = np.zeros(matrix.shape)
-    for p in range(nbP):
-        ret[:,p,0] = procrustesScaleVector(matrix[:,p,0])
-        ret[:,p,1] = procrustesScaleVector(matrix[:,p,1])
-        
-    return ret
-
-'''
-Takes numpy vector with the values for one persons, different x- or y values of landmarks, and returns a similar vector so that the standard deviation
-of the values becomes 1.
-'''    
-def procrustesScaleVector(vector):
-    nbL = vector.shape[0]
-    std = np.std(vector)
-    
-    ret = np.zeros(vector.shape)
-    for l in range(nbL):
-        ret[l] = vector[l]/std
-    return ret
-
-def distance(matrix1,matrix2):
-    return 0.5 #TODO
-
-def alignWith(matrix,mean):
-    return matrix #TODO
-
-def newMean(itmat, firstMean):
-    return firstMean #TODO: berekenen, alignen met firstMean en schalen
-
-def procrustesMatrix(matrix):
-    # translate to gravity
-    itmat = procrustesTranslateMatrix(matrix)
-    # choose one example, scale it
-    firstMean = np.zeros(matrix[:,0,:].shape)
-    firstMean[:,0,0] = procrustesScaleVector(itmat[:,0,0])
-    firstMean[:,0,1] = procrustesScaleVector(itmat[:,0,1])
-    # loop
-    converged = False
-    newmean = firstMean
-    while not converged:
-        oldmean = newmean
-        # align all schapes
-        itmat = alignWith(itmat, oldmean)
-        # calculate newmean
-        newmean = newMean(itmat, firstMean)  
-        # update converged
-        converged = distance(oldmean, newmean) < 0.5
-    return itmat
+import procrustes
 
 '''
 Creates a  tooth matrix from the landmarks in the files.
@@ -97,5 +18,6 @@ def readLandmarks(toothId, nbPersons, nbLandmarks):
     return landmarks
     
 if __name__ == '__main__':
-    print procrustesMatrix(readLandmarks(1, 14, 80))
+    lm = readLandmarks(1, 14, 80)
+    print procrustes.procrustesMatrix(lm)
     
---
+++ cvfinal/src/procrustes.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+'''
+Takes toothmatrix, and returns a similar one with, for every person, the landmarks in coordinates with an origin in the mean of those landmarks.
+'''
+def procrustesTranslateMatrix(matrix):
+    nbP = matrix.shape[1]
+    ret = np.zeros(matrix.shape)
+    for p in range(nbP):
+        ret[:,p,0] = procrustesTranslateVector(matrix[:,p,0])
+        ret[:,p,1] = procrustesTranslateVector(matrix[:,p,1])
+        
+    return ret #TODO: methode gebruikt?
+
+'''
+Takes numpy vector with the values for one persons, different x- or y values of landmarks, and returns a similar vector with the delta of each
+entry to the average over the landmarks.
+'''    
+def procrustesTranslateVector(vector):
+    nbL = vector.shape[0]
+    avg = np.mean(vector)
+    
+    ret = np.zeros(vector.shape)
+    for l in range(nbL):
+        ret[l] = vector[l] - avg
+    return ret
+
+'''
+Takes toothmatrix, and returns a similar one with, for every person, the landmarks in coordinates scaled so
+that the standard deviation of those landmarks becomes 1 (in both directions).
+'''
+def procrustesScaleMatrix(matrix):
+    nbP = matrix.shape[1]
+    ret = np.zeros(matrix.shape)
+    for p in range(nbP):
+        ret[:,p,0] = procrustesScaleVector(matrix[:,p,0])
+        ret[:,p,1] = procrustesScaleVector(matrix[:,p,1])
+        
+    return ret
+
+'''
+Takes numpy vector with the values for one persons, different x- or y values of landmarks, and returns a similar vector so that the standard deviation
+of the values becomes 1.
+'''    
+def procrustesScaleVector(vector):
+    nbL = vector.shape[0]
+    std = np.std(vector)
+    
+    ret = np.zeros(vector.shape)
+    for l in range(nbL):
+        ret[l] = vector[l]/std
+    return ret
+
+def distance(matrix1,matrix2):
+    return 0.5 #TODO: afstand berekenen
+
+def alignWith(matrix,mean):
+    #TODO: roteren
+    return procrustesScaleMatrix(matrix)
+
+def estimateMean(itmat, firstMean):
+    nbL = itmat.shape[0]
+    newMatrix = np.zeros((nbL,1,2))
+    
+    # calculate mean (as toothmatrix with one person)
+    for l in range(nbL):
+        avgX = np.average(itmat[l,:,0])
+        avgY = np.average(itmat[l,:,1])
+        newMatrix[l,0,0] = avgX
+        newMatrix[l,0,1] = avgY
+    
+    # align new mean with first mean
+    newMatrix = alignWith(newMatrix, firstMean)
+    
+    # return new mean as 2D matrix
+    newMean = newMatrix[:,0,:]
+    return newMean
+
+def procrustesMatrix(matrix):    
+    # translate to gravity
+    itmat = procrustesTranslateMatrix(matrix)
+    
+    print procrustesScaleMatrix(itmat)
+    
+    # choose one example, scale it
+    firstMean = np.zeros(matrix[:,0,:].shape)
+    firstMean[:,0] = procrustesScaleVector(itmat[:,0,0])
+    firstMean[:,1] = procrustesScaleVector(itmat[:,0,1])
+    
+    # loop
+    converged = False
+    newmean = firstMean
+    it = 0
+    while not converged:
+        oldmean = newmean
+        # align all schapes
+        itmat = alignWith(itmat, oldmean)
+        # calculate newmean
+        newmean = estimateMean(itmat, firstMean)  
+        # update converged
+        converged = distance(oldmean, newmean) < 0.5 or it > 100
+        it = it + 1
+    return itmat
+
