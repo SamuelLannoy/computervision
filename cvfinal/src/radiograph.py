60d748f8f2e04de7f6f418ef1e326788cdda4b4a
Samuel Lannoy <samuel.lannoy@student.kuleuven.be>
2014-05-20 19:16:43 +0200

    radiograaf

--- cvfinal/src/radiograph.py
+++ cvfinal/src/radiograph.py
@@ -8,18 +8,19 @@ import numpy as np
 import cv2
 
 def preprocess(img):
-    clahe = cv2.createCLAHE();
-    return clahe.apply(img)
+    cropped = img[400:1600,1000:2100]
+    denoised = cv2.fastNlMeansDenoising(cropped)
+    clahe = cv2.createCLAHE(clipLimit=5);
+    return clahe.apply(denoised)
 
 def sharpen(img):
-    kernel = np.array([[0,0,0], [0,2,0], [0,0,0]]) - (1/9) * np.array([[1,1,1], [1,1,1], [1,1,1]])
+    kernel = np.array([[0,0,0], [0,1.25,0], [0,0,0]]) - (1/9) * np.array([[1,1,1], [1,1,1], [1,1,1]])
     return cv2.filter2D(img, -1, kernel)
 
 '''
 Everything starting here is just a test that can be run
 '''
-img = cv2.imread('../data/Radiographs/01.tif',0)
-cv2.imshow('original', img)
+img = cv2.imread('../data/Radiographs/07.tif',0)
+#cv2.imshow('original', img)
 cv2.imshow('preprocessed', preprocess(img))
-#cv2.imshow('sharpened', sharpen(preprocess(img)))
 cv2.waitKey(0)
