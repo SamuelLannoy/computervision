a7a7415ef3c0969d3dc443f5e01d2bbbed252ea0
Vital D'haveloose <vital.dhaveloose@hotmail.be>
2014-05-20 19:12:31 +0200

    skeleton of profile (matching)

---
+++ cvfinal/src/profile.py
@@ -0,0 +1,28 @@
+'''
+Created on 20-mei-2014
+
+@author: vital.dhaveloose
+'''
+
+'''
+Returns the mean of the profiles of the images (g striped), and the covariance matrix (S_g).
+'''
+def getModel(imgs, point, direction, N):
+
+
+'''
+Returns an array of 2*N+1 grey values that represent the
+profile in the given image, at the given point in the given direction.
+'''
+def getProfile(img, point, direction, N):
+    
+
+'''
+Returns an integer that indicates the translation of the profileToMatch
+to the learnedProfile so that the Mahalanobis distance becomes minimal.
+'''
+def matchProfile(learnedProfile, profileToMatch):
+    
+    
+
+    
